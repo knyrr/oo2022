7f25e9a9c462431558c57636976b42f0c114ddf2
Martin Rünk <90237413+knyrr@users.noreply.github.com>
2022-02-07 14:35:46 +0200

    Create Funktsioonid1.java

---
+++ 01_funktsioon/Funktsioonid1.java
@@ -0,0 +1,15 @@
+public class Funktsioonid1{
+	public static double sentimeetriteks(double tollid){
+		return tollid*2.54;
+	}
+		public static double tollideks(double sentimeetrid){
+		return sentimeetrid*0.3937;
+	}
+	public static void main(String[] args){
+		System.out.println(tollideks(10));
+		if(args.length>0){
+			double t=Double.parseDouble(args[0]);
+			System.out.println(sentimeetriteks(t));
+		}
+	}
+}
