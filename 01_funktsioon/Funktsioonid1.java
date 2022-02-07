eff6a8c319803bc78c3fceca84f2e577e037673c
Martin Rünk <90237413+knyrr@users.noreply.github.com>
2022-02-07 14:36:38 +0200

    Update Funktsioonid1.java

--- 01_funktsioon/Funktsioonid1.java
+++ 01_funktsioon/Funktsioonid1.java
@@ -2,7 +2,7 @@ public class Funktsioonid1{
 	public static double sentimeetriteks(double tollid){
 		return tollid*2.54;
 	}
-		public static double tollideks(double sentimeetrid){
+	public static double tollideks(double sentimeetrid){
 		return sentimeetrid*0.3937;
 	}
 	public static void main(String[] args){
